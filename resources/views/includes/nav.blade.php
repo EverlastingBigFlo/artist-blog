dc8018d4e4a44ec3429776ad6191beba20ff4617
EverlastingLilflo <anjorin199@gmail.com>
2023-11-22 22:27:38 +0100

    added nav content

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,8 +1,12 @@
 <div>
-    <ul>
-        <li style="color: #49676E">LOGO</li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
+    <div>LOGO</div>
+    <div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Galleries</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+    </div>
+    <div></div>
 </div>
