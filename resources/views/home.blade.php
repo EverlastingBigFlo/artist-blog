17de45667e7a2a430ffc14c35c47fc3a23cb3f1e
EverlastingLilflo <anjorin199@gmail.com>
2023-11-24 00:38:14 +0100

    added another section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,24 +11,46 @@
             <img src="{{ asset('img/Rectangle 6.png') }}" alt="">
             <img src="{{ asset('img/Rectangle 7.png') }}" alt="">
         </div>
+    </section>
 
-        {{-- write up section --}}
+    <section>
 
-        <div class="meanSection">
+         {{-- write up section --}}
+
+         <div class="meanSection">
             <h1>
                 MY ART MEANS TO ME
             </h1>
             <p>
                 What is Lorem Ipsum?
+                <br>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a
+                industry's standard dummy <br>
+                text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a <br>
                 type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-                including
+                remaining essentially <br>
+                unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently <br>
+                with desktop publishing software like Aldus PageMaker including <br>
                 versions of Lorem Ipsum.
             </p>
+        </div>
+    </section>
+
+    {{-- subscribe section --}}
+    <section>
+        <div class="subscribeSec">
+            <h1>
+                NEWSLETTER
+            </h1>
+            <h6>
+                A monthly newsletter keeping you up yo date.
+            </h6>
+
+            <div class="form">
+
+            </div>
         </div>
     </section>
 @endsection
