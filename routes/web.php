8badae8ec500b1fa6e839cfaf591c029b3302044
EverlastingLilflo <anjorin199@gmail.com>
2023-11-23 14:06:07 +0100

    added footer and changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\IndexCntroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [IndexCntroller::class,'index'])->name('index');
+
