7d712acf0458a0bfe04d1d46d55cfc7d3a0be58d
EverlastingLilflo <anjorin199@gmail.com>
2023-11-23 14:19:24 +0100

    footer contents added

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,12 @@
 <div class="footerBg">
-    
+    <div class="footerCont">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+    </div>
 </div>
