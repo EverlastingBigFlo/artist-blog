5aa219d75b9ce0d1711a7e7582398edb64bb2c6e
EverlastingLilflo <anjorin199@gmail.com>
2023-11-24 00:04:12 +0100

    added new section and styled it

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,34 @@
 @extends('/layouts/layout')
 
 @section('content')
-
-<section>
-    <div class="heroSection">
-        <img src="{{asset('img/Rectangle 4.png')}}" alt="">
-    </div>
-        <div class="anotherSection">
-
+    <section>
+        <div class="heroSection">
+            <img src="{{ asset('img/Rectangle 4.png') }}" alt="">
         </div>
-</section>
 
+        {{-- second image section --}}
+        <div class="anotherSection">
+            <img src="{{ asset('img/Rectangle 6.png') }}" alt="">
+            <img src="{{ asset('img/Rectangle 7.png') }}" alt="">
+        </div>
+
+        {{-- write up section --}}
+
+        <div class="meanSection">
+            <h1>
+                MY ART MEANS TO ME
+            </h1>
+            <p>
+                What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a
+                type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+                including
+                versions of Lorem Ipsum.
+            </p>
+        </div>
+    </section>
 @endsection
