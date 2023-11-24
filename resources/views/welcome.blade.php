fff50b2681b740db9f33f1150ca67d004eb10334
EverlastingLilflo <anjorin199@gmail.com>
2023-11-24 11:54:15 +0100

    using internal styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,36 @@
     <title>Welcome Page</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Cormorant Garamond', serif;
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        .navBg {
+            max-heightht: 101px;
+        }
+
+        .navBg .logo a {
+            text-decoration: none;
+            font-size: 50px;
+            color: #49676E;
+            padding-left: 7rem;
+            padding-top: 5rem;
+            transform: translateY(5rem);
+        }
+
+        .navBg .logo a:hover {
+            color: black;
+        }
+    </style>
 </head>
+
 <body>
 
     <header>
@@ -17,4 +46,5 @@
         </div>
     </header>
 </body>
+
 </html>
