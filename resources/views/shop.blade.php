eea5141078c3ad2e93d132c2d2c6368cfc4a1791
EverlastingLilflo <anjorin199@gmail.com>
2023-11-28 15:00:10 +0100

    added header and gave my container width and height

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,9 +1,22 @@
 <style>
+    .heroWrapper {
+        min-height: 100vh;
+        max-width: 100vw;
+    }
 
+    .heroWrapper .heroCont {
+        max-width: 80%;
+    }
 </style>
 
 @extends('layouts/layout')
 
 @section('content')
-    
+    <div class="heroWrapper">
+        <div class="heroCont">
+            <div class="subHeader">
+                <p>Shop</p>
+            </div>
+        </div>
+    </div>
 @endsection
