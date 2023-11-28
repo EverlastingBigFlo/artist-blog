5f290314dd419531cb74b06fc19f461172a7f97b
EverlastingLilflo <anjorin199@gmail.com>
2023-11-28 12:53:24 +0100

    using flex instead of grid

--- resources/views/galleries.blade.php
+++ resources/views/galleries.blade.php
@@ -1,67 +1,38 @@
-@extends('/layouts/layout')
-{{-- @extends('/layouts/layout') --}}
 <style>
-    .gridWrapper{
-    max-width: 80vw;
-    margin: auto;
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-auto-rows: minmax(100px, 200px);
-    grid-gap: 30px;
-    grid-template-areas:
-        "box1 box1 box2 box2"
-        "box1 box1 box2 box2"
-        "box1 box1 box3 box3"
-        "box4 box4 box4 box4"
-        "box4 box4 box4 box4"
-        "box5 box5 box6 box6";
+    .heroBg {
+        margin: auto;
     }
 
-    .box1{
-        background-color: green;
-        grid-area: box1;
+    .heroBg .header {
+        display: flex;
+        align-content: center;
+        justify-content: center;
     }
-    .box2{
-        background-color: green;
-        grid-area: box2;
 
-    }
-    .box3{
-        background-color: green;
-        grid-area: box3;
-
-    }
-    .box4{
-        background-color: green;
-        grid-area: box4;
-
-    }
-    .box5{
-        background-color: green;
-        grid-area: box5;
-    }
-    .box6{
-        background-color: green;
-        grid-area: box6;
+    .firstGrid {
+        display: flex;
+        gap: 4rem;
     }
 </style>
 
+@extends('layouts/layout');
 
 @section('content')
-    
-    <div class="subHeader">
-        <p>
-            Gallery
-        </p>
-    </div>
+    <section class="heroBg">
+        <div class="header">
+            <h1>Gallery</h1>
 
-    <div class="gridWrapper">
-        <div class="box1"></div>
-        <div class="box2"></div>
-        <div class="box3"></div>
-        <div class="box4"></div>
-        <div class="box5"></div>
-        <div class="box6"></div>
-    </div>
-   </div>
+        </div>
+        <div class="heroSection">
+            <div class="firstGrid">
+                <div><img src="{{ asset('asset/img/Rectangle 16.png') }}" alt=""></div>
+                <div>
+                    <div><img src="{{ asset('asset/img/Rectangle 17.png') }}" alt=""></div>
+
+                    <div><img src="{{ asset('asset/img/Rectangle 18.png') }}" alt=""></div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
