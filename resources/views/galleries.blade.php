ea78d088fdcd610b2d771e0cd091495857e7c7dc
EverlastingLilflo <anjorin199@gmail.com>
2023-11-28 13:01:05 +0100

    styling

--- resources/views/galleries.blade.php
+++ resources/views/galleries.blade.php
@@ -7,9 +7,15 @@
         display: flex;
         align-content: center;
         justify-content: center;
+        /* height: 5rem; */
+        padding: 5rem 0;
+    }
+    .heroBg .header p{
+       color: #49676E;
+       font-size: 40px;
     }
 
-    .firstGrid {
+    .heroBg .heroSection .firstGrid {
         display: flex;
         gap: 4rem;
     }
@@ -20,8 +26,8 @@
 @section('content')
     <section class="heroBg">
         <div class="header">
-            <h1>Gallery</h1>
-
+            <p>Gallery</p>
+            467
         </div>
         <div class="heroSection">
             <div class="firstGrid">
