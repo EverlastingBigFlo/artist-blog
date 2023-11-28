47fb753fd165ff5a2b07e1a580a30d85686885e7
EverlastingLilflo <anjorin199@gmail.com>
2023-11-28 13:55:57 +0100

    changing my content

--- resources/views/galleries.blade.php
+++ resources/views/galleries.blade.php
@@ -1,6 +1,35 @@
+<style>
+    .heroWrapper {
+        height: 100vh;
+    }
+
+    .heroWrapper .gridWrapper {
+        width: 90vw;
+        margin: auto;
+    }
+
+    .heroWrapper .gridWrapper .subHeader {
+        height: 10rem;
+        display: flex;
+        justify-content: center;
+        align-items: center
+    }
+
+    .heroWrapper .gridWrapper .subHeader p {
+        color: #49676E;
+        font-size: 40px
+
+    }
+</style>
+
 @extends('layouts/layout')
 
 @section('content')
-    
-
+    <div class="heroWrapper">
+        <div class="gridWrapper">
+            <div class="subHeader">
+                <p>Gallery</p>
+            </div>
+        </div>
+    </div>
 @endsection
