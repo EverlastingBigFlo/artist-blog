4799f83ffbd42d0372e7d9f40b2ea26e7ae7de05
EverlastingLilflo <anjorin199@gmail.com>
2023-12-02 17:35:23 +0100

    created section and added image to it

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,4 +1,26 @@
-@extends('layouts/layout')
-@section('content')
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+    }
+    .wrapper{
+        background-color: #202227;
+        max-width: 100vw;
+        max-height: 100vh;
+        
+    }
+</style>
+
     
+
+
+@section('content')
+<section class="wrapper">
+    <div class="contentWrapper">
+        <div class="content"></div>
+        <div class="image">
+            <img src="{{asset('asset/img/Frame 1 (1).png')}}" alt="image">
+        </div>
+    </div>
+</section>
 @endsection
