29ccdebf75083a605f3277561a74c3337b441e88
EverlastingLilflo <anjorin199@gmail.com>
2023-12-02 17:42:22 +0100

    removed content and added my new docs type

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,26 +1,38 @@
 <style>
-    *{
+    * {
         padding: 0;
         margin: 0;
     }
-    .wrapper{
+
+    .wrapper {
         background-color: #202227;
         max-width: 100vw;
         max-height: 100vh;
-        
+
     }
 </style>
 
-    
 
 
-@section('content')
-<section class="wrapper">
-    <div class="contentWrapper">
-        <div class="content"></div>
-        <div class="image">
-            <img src="{{asset('asset/img/Frame 1 (1).png')}}" alt="image">
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+</head>
+
+<body>
+    <section class="wrapper">
+        <div class="contentWrapper">
+            <div class="content"></div>
+            <div class="image">
+                <img src="{{ asset('asset/img/Frame 1 (1).png') }}" alt="image">
+            </div>
         </div>
-    </div>
-</section>
-@endsection
+    </section>
+</body>
+
+</html>
