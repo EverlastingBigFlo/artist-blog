eb5f93a227cde0974b7b2e1ef44ab7934f6a0f89
EverlastingLilflo <anjorin199@gmail.com>
2023-12-03 09:46:08 +0100

    added icons and created an algorithm to hash my password on click of it

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,9 +7,7 @@
     <link rel="stylesheet" href="{{asset('asset/css/register.css')}}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 </head>
 <body>
    <section>
@@ -25,24 +23,44 @@
                     </h6>
                 </div>
                 <label for="fullName">Full Name</label>
-                <input type="text" id="fullName" name="fullName" required>
+                <input type="text" id="fullName" name="fullName" placeholder="Enter your full name" required>
 
                 <label for="email">Email Address</label>
-                <input type="email" id="email" name="email" required>
+                <input type="email" id="email" name="email" placeholder="Enter your email address" required>
 
                 <label for="password">Create Password</label>
-                <input type="password" id="password" name="password" required>
+                <div class="password-container">
+                    <input type="password" id="password" name="password" placeholder="Enter your password" required>
+                    <span class="eye-icon" onclick="togglePasswordVisibility()">
+                        
+                    </span>
+                </div>
 
                 <button type="submit">Register</button>
             </form>
             <hr>
             <button id="google-btn">
-                <img src="google-logo.png" alt="Google Logo" id="google-logo">
+                <img src="{{asset('asset/icons/flat-color-icons_google.png')}}" alt="Google Logo" id="google-logo">
                 Register with Google
             </button>
         </div>
         <img id="bgImg" src="{{asset('asset/img/Frame 1 (1).png')}}" alt="Your Image" style="border-radius: 8px;">
     </div>
+
+    <script>
+        function togglePasswordVisibility() {
+            var passwordInput = document.getElementById("password");
+            var eyeIcon = document.querySelector(".eye-icon");
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                // eyeIcon.innerHTML = "&#128064;"; // Closed eye
+            } else {
+                passwordInput.type = "password";
+                // eyeIcon.innerHTML = "&#128065;"; // Open eye
+            }
+        }
+    </script>
    </section>
 </body>
 </html>
