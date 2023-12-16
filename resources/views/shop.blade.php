5d602ef8c91c56ee2b5f6547ad700af14638469b
EverlastingLilflo <anjorin199@gmail.com>
2023-12-16 10:42:24 +0100

    changed

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -49,38 +49,38 @@
                     <p id="searchIcon">🔍</p>
                 </div>
             </div>
-        
-            <div>
+
+            <div class="p-10">
                 <div class="flex justify-between">
                     <div class="flex items-center flex-col">
-                        <img src="{{asset('asset/img/Rectangle 32.png')}}" alt="">
+                        <img src="{{ asset('asset/img/Rectangle 32.png') }}" alt="">
                         <h4 class="text-[30px] text-[49676E] p-5">Heading</h4>
                         <p class="text-[25px] text-[49676E]">₹ 500</p>
                     </div>
                     <div class="flex items-center flex-col">
-                        <img src="{{asset('asset/img/Rectangle 33.png')}}" alt="">
+                        <img src="{{ asset('asset/img/Rectangle 33.png') }}" alt="">
                         <h4 class="text-[30px] text-[49676E] p-5">Heading</h4>
                         <p class="text-[25px] text-[49676E]">₹ 500</p>
                     </div>
                     <div class="flex items-center flex-col">
-                        <img src="{{asset('asset/img/Rectangle 34.png')}}" alt="">
+                        <img src="{{ asset('asset/img/Rectangle 34.png') }}" alt="">
                         <h4 class="text-[30px] text-[49676E] p-5">Heading</h4>
                         <p class="text-[25px] text-[49676E]">₹ 500</p>
                     </div>
                 </div>
                 <div class="flex justify-between">
                     <div class="flex items-center flex-col">
-                        <img src="{{asset('asset/img/Rectangle 35.png')}}" alt="">
+                        <img src="{{ asset('asset/img/Rectangle 35.png') }}" alt="">
                         <h4 class="text-[30px] text-[49676E] p-5">Heading</h4>
                         <p class="text-[25px] text-[49676E]">₹ 500</p>
                     </div>
                     <div class="flex items-center flex-col">
-                        <img src="{{asset('asset/img/Rectangle 36.png')}}" alt="">
+                        <img src="{{ asset('asset/img/Rectangle 36.png') }}" alt="">
                         <h4 class="text-[30px] text-[49676E] p-5">Heading</h4>
                         <p class="text-[25px] text-[49676E]">₹ 500</p>
                     </div>
                     <div class="flex items-center flex-col">
-                        <img src="{{asset('asset/img/Rectangle 37.png')}}" alt="">
+                        <img src="{{ asset('asset/img/Rectangle 37.png') }}" alt="">
                         <h4 class="text-[30px] text-[49676E] p-5">Heading</h4>
                         <p class="text-[25px] text-[49676E]">₹ 500</p>
                     </div>
