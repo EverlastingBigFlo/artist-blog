037f62bcba11efc53cf0ac9b8aae11f1ed32ce6a
EverlastingLilflo <anjorin199@gmail.com>
2023-12-16 10:42:59 +0100

    changed my code from css to tailwindcss

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,26 +1,4 @@
 <style>
-    .heroWrapper {
-        min-height: 100vh;
-        max-width: 100vw;
-    }
-
-    .heroWrapper .heroCont {
-        max-width: 80%;
-        margin: auto;
-    }
-
-    .heroWrapper .heroCont .subHeader {
-        height: 10rem;
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-    }
-
-    .heroWrapper .heroCont .subHeader p {
-        color: #49676E;
-        font-size: 40px;
-    }
-
     .heroWrapper .heroCont .subHeader input[type="search"] {
         width: 20rem;
         opacity: 1;
@@ -40,10 +18,10 @@
 @extends('layouts/layout')
 
 @section('content')
-    <div class="heroWrapper">
-        <div class="heroCont">
-            <div class="subHeader">
-                <p>Shop</p>
+    <div class="heroWrapper min-h-full max-w-[100vw] ">
+        <div class="heroCont max-w-[80%] m-auto ">
+            <div class="subHeader h-40 flex justify-around items-center">
+                <p class="text-[49676E] text-[40px]">Shop</p>
                 <div style="display: flex;">
                     <input type="search" id="searchInput" placeholder="Search" class="active">
                     <p id="searchIcon">🔍</p>
