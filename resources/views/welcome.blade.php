024444941278aa4097093928bea6a26d1b2eabfd
EverlastingLilflo <anjorin199@gmail.com>
2023-12-18 22:49:02 +0100

    changing welcome screen code and writing media query

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet">
-    <style>
+    {{-- <style>
         * {
             font-family: 'Cormorant Garamond', serif;
             padding: 0;
@@ -29,7 +29,7 @@
         }
 
         .welcomePage header .navBg {
-            max-heightht: 100%;
+            max-height: 100%;
             margin-top: 4rem;
         }
 
@@ -87,16 +87,23 @@
             box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
 
         }
-    </style>
+    </style> --}}
+@vite('resources/css/app.css')
 </head>
 
 <body>
     {{-- max-w-prose max-h-full sm:max-w-full sm:max-h-screen --}}
-    <div class="welcomePage  ">
+    <div class="welcomePage flex m-auto border-[2.5rem] border-black sm:h-screen md:h-screen lg:h-screen xl:h-screen 2xl:h-screen sm:w-screen md:w-screen lg:w-screen xl:w-screen 2xl:w-screen">
         {{-- nav --}}
         <header>
-            <div class="navBg">
-                <div class="logo hover:text-bla1"><a href="{{asset('/')}}">LOGO</a></div>
+            <div class="navBg sm:max-h-full md:max-h-full lg:max-h-full xl:max-h-full 2xl:max-h-full mt-16">
+                <div class="logo hover:text-bla1"><a class=" text-5xl text-[#49676E] hover:text-black" href="{{asset('/')}}">LOGO</a></div>
+                text-decoration: none;
+                font-size: 50px;
+                color: #49676E;
+                padding-left: 5rem;
+                padding-top: 5rem;
+                transform: translateY(5rem);
             </div>
         </header>
 
@@ -105,7 +112,7 @@
         <section>
             <div class="heroSection">
                 <div class="largeImage">
-                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="{{ asset('asset/img/Ellipse 1.png') }}" alt="largeImage">
+                    <img class=""src="{{ asset('asset/img/Ellipse 1.png') }}" alt="largeImage">
                 </div>
                 <div class="smallImage">
                     <img src="{{ asset('asset/img/Ellipse 3.png') }}" alt="smallImage">
